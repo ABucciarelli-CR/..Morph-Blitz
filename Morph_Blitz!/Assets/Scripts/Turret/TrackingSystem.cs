7d59d30d3540989a21699d99cc7ffe5637ef23d2
DESKTOP-BA831AE\Alin <dragomir.alinmihai@gmail.com>
2017-04-29 16:21:56 +0200

    _ Fixed Turret TrackingSystem script

--- Morph_Blitz!/Assets/Scripts/Turret/TrackingSystem.cs
+++ Morph_Blitz!/Assets/Scripts/Turret/TrackingSystem.cs
@@ -15,20 +15,14 @@ public class TrackingSystem : MonoBehaviour
 	}
 
 	// Update is called once per frame
-	void Update () 
-	{
-		if(m_target)
-		{
-			print (m_target.transform.position);
-			if(m_lastKnownPosition != m_target.transform.position)
-			{
-				
+	void Update () {
+		if(m_target){
+			if(m_lastKnownPosition != m_target.transform.position){
 				m_lastKnownPosition = m_target.transform.position;
-				m_lookAtRotation = Quaternion.LookRotation(m_lastKnownPosition - transform.position, transform.forward);
+				m_lookAtRotation = Quaternion.LookRotation(m_lastKnownPosition - transform.position);
 			}
 
-			if(transform.rotation != m_lookAtRotation)
-			{
+			if(transform.rotation != m_lookAtRotation){
 				transform.rotation = Quaternion.RotateTowards(transform.rotation, m_lookAtRotation, speed * Time.deltaTime);
 			}
 		}
