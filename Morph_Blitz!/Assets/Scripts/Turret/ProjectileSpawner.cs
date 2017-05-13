45f403dfebf2610344da01c2293c89ff78dd277f
ABucciarelli-CR <alessiobuccia@gmail.com>
2017-05-13 17:51:27 +0200

    _ Adesso i le torrette si attivano con i rispettivi colori _

--- Morph_Blitz!/Assets/Scripts/Turret/ProjectileSpawner.cs
+++ Morph_Blitz!/Assets/Scripts/Turret/ProjectileSpawner.cs
@@ -6,21 +6,33 @@ public class ProjectileSpawner : MonoBehaviour {
 
 	public GameObject enemyPrefab;
 	public GameObject spawnPoint;
+	public GameObject EnemySpawn; //define the enemy controller
+
 	public float spawnTime = 3f;
 
 	private GameObject _EN;
 	private float _comparedTime = 3f;
 	[SerializeField]private float _shootPower = 50f;
 
+	void Start()
+	{
+		EnemySpawn = GameObject.Find ("EnemySpawnController");
+	}
+
 	void Update () 
 	{
-		if (_comparedTime <=0) 
+
+		if (gameObject.CompareTag ("TowerType1") && EnemySpawn.CompareTag ("EnemyType1ON") || gameObject.CompareTag ("TowerType2") && EnemySpawn.CompareTag ("EnemyType2ON") || gameObject.CompareTag ("TowerType3") && EnemySpawn.CompareTag ("EnemyType3ON")) 
 		{
-			_EN = Instantiate (enemyPrefab, spawnPoint.transform.position, Quaternion.identity);
-			_EN.GetComponent<Rigidbody> ().AddRelativeForce (transform.forward * _shootPower);
-			_comparedTime = spawnTime;
+			Debug.Log ("Christiana Capotondi");
+			if (_comparedTime <= 0) 
+			{
+				_EN = Instantiate (enemyPrefab, spawnPoint.transform.position, Quaternion.identity);
+				_EN.GetComponent<Rigidbody> ().AddRelativeForce (transform.forward * _shootPower);
+				_comparedTime = spawnTime;
+			}
+			_comparedTime -= Time.deltaTime;
 		}
-		_comparedTime -= Time.deltaTime;
 	}
 
 }
