f9c56f6c902c57e6796ea4f5ff8780caabcbd0d3
DESKTOP-BA831AE\Alin <dragomir.alinmihai@gmail.com>
2017-05-13 17:10:49 +0200

    _Added Prefab TurretEN1, TurretEN2, TurretEN3 and scripts

---
+++ Morph_Blitz!/Assets/Scripts/Turret/ProjectileSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileSpawner : MonoBehaviour {
+
+	public GameObject enemyPrefab;
+	public GameObject spawnPoint;
+	public float spawnTime = 3f;
+
+	private GameObject _EN;
+	private float _comparedTime = 3f;
+	[SerializeField]private float _shootPower = 50f;
+
+	void Update () 
+	{
+		if (_comparedTime <=0) 
+		{
+			_EN = Instantiate (enemyPrefab, spawnPoint.transform.position, Quaternion.identity);
+			_EN.GetComponent<Rigidbody> ().AddRelativeForce (transform.forward * _shootPower);
+			_comparedTime = spawnTime;
+		}
+		_comparedTime -= Time.deltaTime;
+	}
+
+}
