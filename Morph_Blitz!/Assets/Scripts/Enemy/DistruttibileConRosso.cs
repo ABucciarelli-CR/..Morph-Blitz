1585e91cde8b939194910f67a28f4d8a428c4bf6
DESKTOP-BA831AE\Alin <dragomir.alinmihai@gmail.com>
2017-06-01 12:57:49 +0200

    Aggiunto prefab e script

---
+++ Morph_Blitz!/Assets/Scripts/Enemy/DistruttibileConRosso.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistruttibileConRosso : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void OnTriggerEnter (Collider col)
+		{
+		if (col.gameObject.CompareTag ("PlayerMod3") || col.gameObject.GetComponent<Rigidbody> ().mass == 0.01f) {
+			//Destroy (gameObject);
+			gameObject.GetComponent<Rigidbody> ().mass = 0.01f;
+
+		} else 
+		{
+			gameObject.GetComponent<Rigidbody> ().mass = 10f;
+		}
+
+		}
+}
