b4879a358dd70d104902466d6af4fcc95c6854cd
Dragomir Alin Mihai <dragomir.alinmihai@gmail.com>
2017-06-29 12:41:36 +0200

    Added spining and rasing (when in range) cube and spin script

---
+++ Morph_Blitz!/Assets/Scripts/Enemy/Spin.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+
+	public float speed = 100f;
+	public GameObject SpiningCube;
+	private bool entered=false;
+	private float currentpos;
+	private float maxheight = 3.0f;
+
+
+	void Awake ()
+	{
+		currentpos = transform.position.y;
+	}
+
+
+	void Update ()
+	{
+		transform.Rotate(Vector3.up, speed * Time.deltaTime);
+		if (entered && transform.position.y < currentpos + maxheight) 
+		{
+			
+			transform.position += Vector3.up * Time.deltaTime;
+			//SpiningCube = new Vector3 (SpiningCube.transform.position.x, SpiningCube.transform.position.y += Time.deltaTime, SpiningCube.transform.position.z);
+		}
+
+	}
+
+	void OnTriggerEnter (Collider col)
+	{
+		entered = true;
+	}
+}
