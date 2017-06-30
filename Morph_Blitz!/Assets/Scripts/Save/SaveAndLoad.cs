1fa9da962d32efec1c671a99ceb5b99d8dda53f8
Alessio Bucciarelli <alessiobuccia@gmail.com>
2017-06-30 14:36:52 +0200

    _ Salvataggio funzionante, caricamento no _

---
+++ Morph_Blitz!/Assets/Scripts/Save/SaveAndLoad.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveAndLoad
+{ 
+
+	public static void Save(GlobalVariables globalVariables)
+	{
+		BinaryFormatter bf = new BinaryFormatter ();
+		FileStream file =  new FileStream (Application.persistentDataPath + "/MorpfSaves.txt", FileMode.Create);;
+
+		DataToSave data = new DataToSave (globalVariables);
+
+		bf.Serialize (file, data);
+		file.Close();
+
+	}
+
+	public static int LoadChangeMusic()
+	{
+		if (File.Exists (Application.persistentDataPath + "/MorpfSaves.txt")) 
+		{
+			BinaryFormatter bf = new BinaryFormatter ();
+			FileStream file = new FileStream (Application.persistentDataPath + "/MorpfSaves.txt", FileMode.Open);
+
+			DataToSave data = bf.Deserialize (file) as DataToSave;
+
+			file.Close();
+
+			return data.changeMusicValue;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+
+	public static bool LoadAudio()
+	{
+		if (File.Exists (Application.persistentDataPath + "/MorpfSaves.txt")) 
+		{
+			BinaryFormatter bf = new BinaryFormatter ();
+			FileStream file = new FileStream (Application.persistentDataPath + "/MorpfSaves.txt", FileMode.Open);
+
+			DataToSave data = bf.Deserialize (file) as DataToSave;
+
+			file.Close();
+
+			return data.audioOn;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	public static float LoadVolume()
+	{
+		if (File.Exists (Application.persistentDataPath + "/MorpfSaves.txt")) 
+		{
+			BinaryFormatter bf = new BinaryFormatter ();
+			FileStream file = new FileStream (Application.persistentDataPath + "/MorpfSaves.txt", FileMode.Open);
+
+			DataToSave data = bf.Deserialize (file) as DataToSave;
+
+			file.Close();
+
+			return data.volume;
+		}
+		else
+		{
+			return 0f;
+		}
+	}
+
+	public static float[] LoadLevelTimeDone()
+	{
+		if (File.Exists (Application.persistentDataPath + "/MorpfSaves.txt")) 
+		{
+			BinaryFormatter bf = new BinaryFormatter ();
+			FileStream file = new FileStream (Application.persistentDataPath + "/MorpfSaves.txt", FileMode.Open);
+
+			DataToSave data = bf.Deserialize (file) as DataToSave;
+
+			file.Close();
+
+			return data.timeDone;
+		}
+		else
+		{
+			return new float[7];
+		}
+	}
+
+}
+
+[Serializable]
+public class DataToSave
+{
+	public bool audioOn;
+
+	public int changeMusicValue;
+
+	public float volume;
+	public float[] timeDone;
+
+	public DataToSave (GlobalVariables globalVariables)
+	{
+		changeMusicValue = globalVariables.changeMusicValue;
+
+		audioOn = globalVariables.audioOn;
+
+		volume = globalVariables.audioVolume;
+
+		timeDone = globalVariables.LevelTimeDone;
+	}
+}
---
+++ Morph_Blitz!/Assets/Scripts/SaveManager/SaveManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SaveManager : MonoBehaviour 
+{
+
+	private int _level = 0;
+	private GlobalVariables globalVariables;
+	private GameObject _global; 
+
+	void Awake()
+	{
+		DontDestroyOnLoad (gameObject);
+
+		if (FindObjectsOfType (GetType ()).Length > 1)
+		{
+			Destroy (gameObject);
+		}
+
+		_global = GameObject.Find("_GlobalVariables");
+		globalVariables = _global.GetComponent<GlobalVariables> ();
+	}
+
+	void Update()
+	{
+		if (_level != SceneManager.GetActiveScene ().buildIndex)
+		{
+			//Debug.Log ("I'm Saving!");
+			_level = SceneManager.GetActiveScene ().buildIndex;
+			SaveAndLoad.Save (globalVariables);
+		}
+	}
+}
